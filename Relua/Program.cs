73c08f862fa2b0727610afa8243538d178880037
lishengjie <lishengjie@elex-tech.com>
2025-05-29 18:43:37 +0800

    重新调整目录结构

---
+++ Relua/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.IO;
+
+namespace Relua {
+    public static class Program {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("----");
+            var testluafile = @"f:/lua2ts/infile.lua";
+            var tokenizer = new Tokenizer(File.ReadAllText(testluafile));
+            var parser = new Parser(tokenizer);
+            var expr = parser.Read();
+            Console.WriteLine($"{expr}");
+            var outpath = "f:/lua2ts/outputfile.ts";
+            File.WriteAllText(outpath,expr.ToString());
+            Console.WriteLine(outpath);
+            
+            // outpath = "f:/lua2ts/outputfile_ts.lua";
+            // File.WriteAllText(outpath,expr.toTSSTring());
+            // Console.WriteLine(outpath);
+            return default;
+        }
+    }
+}
